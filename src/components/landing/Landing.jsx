62419009eab835e7146bfc648f850954488f4c53
gtz.jesus <gtz.jesus@outlook.com>
2024-02-20 18:05:55 -0700

    New Landing component added

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -16,13 +16,62 @@ import styled from 'styled-components';
 // This section has all CSS styles configured for every HTML element.
 const StyledLanding = styled.div`
   height: 200vh;
+  padding: var(--height-navigation) 0;
+`;
+
+const Title = styled.span`
+  font-size: var(--font-large);
+  padding: 0 var(--padding-small);
+`;
+
+const Scroll = styled.div`
+  // Code logic to arrange children
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  // Code logic for positioning fixed in all web app + header config
+  width: var(--width-full-window);
+
+  // Padding for all
+  padding: var(--padding-small) 0;
+`;
+
+const SubTitle = styled.span`
+  font-size: var(--font-xxxsmall);
+  padding-left: var(--padding-small);
+`;
+
+const Hint = styled.span`
+  font-size: var(--font-xxxsmall);
+  padding-right: var(--padding-small);
+`;
+
+const Realtor = styled.div`
+  // Code logic for setting the background
+  // Design background from Figma software
+  background: url('backgrounds/jesica.jpg');
+  background-size: cover;
+
+  height: 50vh;
 `;
 // ------------------------------
 // Component
 // ------------------------------
 // This section has our React Component which handles the hook data
 function Landing() {
-  return <StyledLanding>hiii</StyledLanding>;
+  return (
+    <StyledLanding>
+      <Title>Jesica Arreola</Title>
+      <br />
+      <Title>Realtor</Title>
+      <br />
+      <Scroll>
+        <SubTitle>(Texas, United States)</SubTitle>
+        <Hint>Scroll Down</Hint>
+      </Scroll>
+      <Realtor></Realtor>
+    </StyledLanding>
+  );
 }
 
 // Export the Component (reusable)
