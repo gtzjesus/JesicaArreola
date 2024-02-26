5d3e70adb99880d99e75f8ebd50f829399eab80f
gtz.jesus <gtz.jesus@outlook.com>
2024-02-26 14:41:48 -0700

    Adding swipe functionality with touch events

--- src/components/houses/Houses.jsx
+++ src/components/houses/Houses.jsx
@@ -4,27 +4,41 @@ import House from './House';
 
 const StyledHouses = styled.div`
   color: var(--color-black);
+  overflow-x: hidden;
 `;
 
 const HousesArea = styled.div`
+  // Code logic to cover the full screen of the device user is in
   margin: 0 auto;
   max-width: var(--width-filled-window);
-  overflow: hidden; /* Ensure the container clips the carousel */
-  position: relative; /* Necessary for absolute positioning of carousel */
 `;
 
 const HousesAreaSlider = styled.div`
-  display: flex;
+  // Code logic for slider (multiple videos)
+  white-space: nowrap;
   transition: ease 1100ms;
 `;
 
 function Houses() {
+  // Code logic for delay carousel
+  const delay = 7500;
+
   const [index, setIndex] = useState(0);
   const [houses, setHouses] = useState([]);
   const [touchStart, setTouchStart] = useState(0);
 
+  const timeoutRef = useRef(null);
+
   const housesAreaRef = useRef(null);
 
+  // Handle reseting the time
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  // Code logic to fetch Houses data from backend when component mounts
   useEffect(() => {
     async function fetchHouses() {
       try {
@@ -32,17 +46,33 @@ function Houses() {
           'https://jesicaarreola-backend-d9d5783c3027.herokuapp.com/api/houses'
         );
         if (!response.ok) {
-          throw new Error('Failed to fetch houses');
+          throw new Error('Failed fo fetch houses');
         }
 
         const data = await response.json();
-        setHouses(data);
+        setHouses(data); // Update the state with fetched houses
       } catch (error) {
         console.log(error);
       }
     }
     fetchHouses();
   }, []);
+
+  // Code logic useEffect react hook for setting the timeout functionality
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === houses.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index, houses]);
 
   const handleTouchStart = (event) => {
     setTouchStart(event.touches[0].clientX);
