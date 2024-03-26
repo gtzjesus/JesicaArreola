cdcf63067285f0d2192706a160ae95918dc59075
gtz.jesus <gtz.jesus@outlook.com>
2024-03-25 18:53:34 -0600

    Configuring frontend with backend post API request to send email from app

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -12,6 +12,8 @@
 // import { useState } from 'react';
 import styled from 'styled-components';
 import Button from '../../ui/buttons/Button';
+import { useState } from 'react';
+import Spinner from '../../ui/spinners/Spinner';
 
 // ------------------------------
 // Styled Componenets
@@ -141,32 +143,32 @@ const FormButton = styled.div`
   margin: var(--margin-small) 0 var(--margin-small) 0;
 `;
 
-// const DeliveryContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   gap: var(--gap-large);
-//   padding: var(--padding-large) 0;
-// `;
+const DeliveryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap-large);
+  padding: var(--padding-large) 0;
+`;
 
-// const Img = styled.img`
-//   max-height: 64px;
-//   max-width: 64px;
-//   margin: 0 auto;
-// `;
+const Img = styled.img`
+  max-height: 64px;
+  max-width: 64px;
+  margin: 0 auto;
+`;
 
-// const Delivered = styled.span`
-//   font-size: var(--font-xsmall);
-// `;
+const Delivered = styled.span`
+  font-size: var(--font-xsmall);
+`;
 
-// const DeliveredSmall = styled.span`
-//   font-size: var(--font-form);
-//   padding: var(--padding-small);
-// `;
+const DeliveredSmall = styled.span`
+  font-size: var(--font-form);
+  padding: var(--padding-small);
+`;
 
-// const ErrorMessage = styled.div`
-//   padding: var(--padding-xxsmall);
-//   color: red;
-// `;
+const ErrorMessage = styled.div`
+  padding: var(--padding-xxsmall);
+  color: red;
+`;
 
 // ------------------------------
 // Component
@@ -179,237 +181,266 @@ function Form() {
   // ------------------------------
   // This section sets all variables needed to implement form submission
   // Create variable to keep track of form submission satus
-  //   const [isSubmittionComplete, setSubmissionComplete] = useState(false);
-  //   // Create variable to keep track of the loading state while submitting form
-  //   const [isLoading, setLoading] = useState(false);
+  const [isSubmittionComplete, setSubmissionComplete] = useState(false);
+  // Create variable to keep track of the loading state while submitting form
+  const [isLoading, setLoading] = useState(false);
 
-  //   // Variables used for error handling
-  //   const [fullName, setFullName] = useState('');
-  //   const [email, setEmail] = useState('');
-  //   const [phoneNumber, setPhoneNumber] = useState('');
-  //   const [formErrors, setFormErrors] = useState('');
+  // Variables used for error handling
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [formErrors, setFormErrors] = useState('');
 
-  //   // Code logic for creating a state for our input data, an object
-  //   const [formData, setFormData] = useState({
-  //     name: '',
-  //     email: '',
-  //     phone: '',
-  //     website: '',
-  //     companyName: '',
-  //     howCanWeHelp: '',
-  //     howDidYouHear: 'default',
-  //   });
+  // Code logic for creating a state for our input data, an object
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    website: '',
+    companyName: '',
+    howCanWeHelp: '',
+    howDidYouHear: 'default',
+  });
 
-  //   // Function to close all modals
-  //   const closeAllModals = () => {
-  //     // Add any other modals you have and set them to false
-  //     setSubmissionComplete(false);
-  //     setLoading(false);
-  //   };
+  // Function to close all modals
+  // const closeAllModals = () => {
+  //   // Add any other modals you have and set them to false
+  //   setSubmissionComplete(false);
+  //   setLoading(false);
+  // };
 
-  //   // Function to close modal after 10 seconds
-  //   const autoCloseModal = () => {
-  //     setTimeout(() => {
-  //       closeAllModals();
-  //     }, 12000); // 10000 milliseconds = 10 seconds
-  //   };
+  // Function to close modal after 10 seconds
+  // const autoCloseModal = () => {
+  //   setTimeout(() => {
+  //     closeAllModals();
+  //   }, 12000); // 10000 milliseconds = 10 seconds
+  // };
 
-  //   // ------------------------------
-  //   // Validation of the FORM
-  //   // ------------------------------
-  //   // This section includes functions used to perform different tasks
+  // ------------------------------
+  // Validation of the FORM
+  // ------------------------------
+  // This section includes functions used to perform different tasks
 
-  //   // Inside validateForm function, set the errors state correctly
-  //   const validateForm = () => {
-  //     const errors = {};
-  //     let isValid = true;
+  // Inside validateForm function, set the errors state correctly
+  const validateForm = () => {
+    const errors = {};
+    let isValid = true;
 
-  //     if (!fullName.trim()) {
-  //       errors.fullName = 'Full name is required';
-  //       isValid = false;
-  //     }
+    if (!fullName.trim()) {
+      errors.fullName = 'Full name is required';
+      isValid = false;
+    }
 
-  //     if (!email.trim()) {
-  //       errors.email = 'Email is required';
-  //       isValid = false;
-  //     }
+    if (!email.trim()) {
+      errors.email = 'Email is required';
+      isValid = false;
+    }
 
-  //     if (!phoneNumber.trim()) {
-  //       errors.phoneNumber = 'Phone number is required';
-  //       isValid = false;
-  //     }
+    if (!phoneNumber.trim()) {
+      errors.phoneNumber = 'Phone number is required';
+      isValid = false;
+    }
 
-  //     // Update the formErrors state
-  //     setFormErrors(errors);
-  //     return isValid;
-  //   };
+    // Update the formErrors state
+    setFormErrors(errors);
+    return isValid;
+  };
 
-  //   // ------------------------------
-  //   // Handler functions
-  //   // ------------------------------
-  //   // This section includes functions used to perform different tasks
+  // ------------------------------
+  // Handler functions
+  // ------------------------------
+  // This section includes functions used to perform different tasks
 
-  //   // Handle the input changes in the form
-  //   // Inside handleInputChange function, update the state correctly
-  //   const handleInputChange = (event) => {
-  //     const { name, value } = event.target;
-  //     // Update the corresponding state variable based on the input name
-  //     if (name === 'name') {
-  //       setFullName(value);
-  //     } else if (name === 'email') {
-  //       setEmail(value);
-  //     } else if (name === 'phone') {
-  //       setPhoneNumber(value);
-  //     }
-  //     // Update the form data object
-  //     setFormData({ ...formData, [name]: value });
-  //   };
+  // Handle the input changes in the form
+  // Inside handleInputChange function, update the state correctly
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    // Update the corresponding state variable based on the input name
+    if (name === 'name') {
+      setFullName(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'phone') {
+      setPhoneNumber(value);
+    }
+    // Update the form data object
+    setFormData({ ...formData, [name]: value });
+  };
 
-  //   // Handle the form submittion
-  //   // Inside handleSubmit function, update the form state properly
-  //   const handleSubmit = async (event) => {
-  //     event.preventDefault();
-  //     setLoading(true);
+  // Handle the form submittion
+  // Inside handleSubmit function, update the form state properly
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
 
-  //     if (validateForm()) {
-  //       try {
-  //         const response = await fetch(
-  //           'https://worldhello-backend-fd375c76bbd5.herokuapp.com/api/sendEmail',
-  //           {
-  //             method: 'POST',
-  //             body: JSON.stringify(formData), // Use formData instead of individual variables
-  //             headers: {
-  //               'Content-Type': 'application/json',
-  //             },
-  //           }
-  //         );
+    if (validateForm()) {
+      try {
+        const response = await fetch(
+          'https://jesicaarreola-backend-d9d5783c3027.herokuapp.com/api/sendEmail',
+          {
+            method: 'POST',
+            body: JSON.stringify(formData), // Use formData instead of individual variables
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
 
-  //         if (response.ok) {
-  //           setSubmissionComplete(true);
-  //         } else {
-  //           console.error('Submission failed:', response.statusText);
-  //         }
-  //       } catch (error) {
-  //         console.error('Error submitting form:', error);
-  //       } finally {
-  //         setLoading(false); // Ensure loading state is always updated
-  //       }
-  //     } else {
-  //       setLoading(false); // Ensure loading state is updated when form validation fails
-  //       console.log('Form validation failed');
-  //     }
-  //   };
+        if (response.ok) {
+          setSubmissionComplete(true);
+        } else {
+          console.error('Submission failed:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error submitting form:', error);
+      } finally {
+        setLoading(false); // Ensure loading state is always updated
+      }
+    } else {
+      setLoading(false); // Ensure loading state is updated when form validation fails
+      console.log('Form validation failed');
+    }
+  };
 
   return (
     <>
-      <StyledForm
-        action="https://worldhello-backend-fd375c76bbd5.herokuapp.com/api/sendEmail"
-        method="POST"
-      >
-        <FormTitle>Let&rsquo;s get in touch</FormTitle>
+      {isLoading ? (
+        <StyledForm>
+          <Spinner />
+        </StyledForm>
+      ) : isSubmittionComplete ? (
+        <>
+          <StyledForm>
+            <DeliveryContainer>
+              <Img src="/icons/email-sent.png"></Img>
+              <Delivered>
+                Email sent successfully! <br />
+                We will reach out to you shortly.
+              </Delivered>
+              <DeliveredSmall>
+                We look forward to create a successful partnership.
+              </DeliveredSmall>
+            </DeliveryContainer>
+          </StyledForm>
+          {/* {autoCloseModal()} */}
+        </>
+      ) : (
+        <>
+          <StyledForm
+            action="https://jesicaarreola-backend-d9d5783c3027.herokuapp.com/api/sendEmail"
+            method="POST"
+          >
+            <FormTitle>Let&rsquo;s get in touch</FormTitle>
 
-        <FormGroup>
+            <FormGroup>
+              <br />
+              <FormSubTitle>
+                just fill the form below{' '}
+                <Icon src="/icons/downarrow.png"></Icon>
+              </FormSubTitle>
+
+              <hr />
+              <FormRow>
+                <Label htmlFor="name">
+                  First and Last Name<Asterik>*</Asterik>
+                </Label>
+                <Input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Enter Full Name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                />
+              </FormRow>
+              {formErrors.fullName && (
+                <ErrorMessage>{formErrors.fullName}</ErrorMessage>
+              )}
+            </FormGroup>
+            <HorizontalGroup>
+              <FormGroup>
+                <FormRow>
+                  <Label htmlFor="email">
+                    Email Address<Asterik>*</Asterik>
+                  </Label>
+                  <Input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="Enter Email Address"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  />
+                </FormRow>
+                {formErrors.email && (
+                  <ErrorMessage>{formErrors.email}</ErrorMessage>
+                )}
+              </FormGroup>
+              <FormGroup>
+                <FormRow>
+                  <Label htmlFor="phone">
+                    Phone Number<Asterik>*</Asterik>
+                  </Label>
+                  <Input
+                    type="text"
+                    id="phone"
+                    name="phone"
+                    placeholder="Enter Phone Number"
+                    value={formData.phone}
+                    onChange={handleInputChange}
+                  />
+                </FormRow>
+                {formErrors.phoneNumber && (
+                  <ErrorMessage>{formErrors.phoneNumber}</ErrorMessage>
+                )}
+              </FormGroup>
+            </HorizontalGroup>
+
+            <FormGroup>
+              <FormRow>
+                <Label htmlFor="howCanWeHelp">How can I help?</Label>
+                <Input
+                  type="text"
+                  id="howCanWeHelp"
+                  name="howCanWeHelp"
+                  placeholder="Need a New Home?"
+                  value={formData.howCanWeHelp}
+                  onChange={handleInputChange}
+                />
+              </FormRow>
+            </FormGroup>
+
+            <FormGroup>
+              <FormRow>
+                <Label htmlFor="howDidYouHear">
+                  How did you hear about me?
+                </Label>
+                <Select
+                  id="howDidYouHear"
+                  name="howDidYouHear"
+                  value={formData.howDidYouHear}
+                  onChange={handleInputChange}
+                >
+                  <Option value="googleSearch">Google</Option>
+                  <Option value="socialMedia">Social Media</Option>
+                  <Option value="friend">Friend</Option>
+                  <Option value="ad">Ad</Option>
+                  <Option value="other">Other</Option>
+                </Select>
+              </FormRow>
+            </FormGroup>
+            <FormButton>
+              <FormRow>
+                <Button type="submit" onClick={handleSubmit}>
+                  Submit
+                </Button>
+              </FormRow>
+            </FormButton>
+          </StyledForm>
           <br />
-          <FormSubTitle>
-            just fill the form below <Icon src="/icons/downarrow.png"></Icon>
-          </FormSubTitle>
-
           <hr />
-          <FormRow>
-            <Label htmlFor="name">
-              First and Last Name<Asterik>*</Asterik>
-            </Label>
-            <Input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter Full Name"
-              //   value={formData.name}
-              //   onChange={handleInputChange}
-            />
-          </FormRow>
-          {/* {formErrors.fullName && (
-            <ErrorMessage>{formErrors.fullName}</ErrorMessage>
-          )} */}
-        </FormGroup>
-        <HorizontalGroup>
-          <FormGroup>
-            <FormRow>
-              <Label htmlFor="email">
-                Email Address<Asterik>*</Asterik>
-              </Label>
-              <Input
-                type="text"
-                id="email"
-                name="email"
-                placeholder="Enter Email Address"
-                // value={formData.email}
-                // onChange={handleInputChange}
-              />
-            </FormRow>
-            {/* {formErrors.email && (
-              <ErrorMessage>{formErrors.email}</ErrorMessage>
-            )} */}
-          </FormGroup>
-          <FormGroup>
-            <FormRow>
-              <Label htmlFor="phone">
-                Phone Number<Asterik>*</Asterik>
-              </Label>
-              <Input
-                type="text"
-                id="phone"
-                name="phone"
-                placeholder="Enter Phone Number"
-                // value={formData.phone}
-                // onChange={handleInputChange}
-              />
-            </FormRow>
-            {/* {formErrors.phoneNumber && (
-              <ErrorMessage>{formErrors.phoneNumber}</ErrorMessage>
-            )} */}
-          </FormGroup>
-        </HorizontalGroup>
-
-        <FormGroup>
-          <FormRow>
-            <Label htmlFor="howCanWeHelp">How can I help?</Label>
-            <Input
-              type="text"
-              id="howCanWeHelp"
-              name="howCanWeHelp"
-              placeholder="Need a New Home?"
-              //   value={formData.howCanWeHelp}
-              //   onChange={handleInputChange}
-            />
-          </FormRow>
-        </FormGroup>
-
-        <FormGroup>
-          <FormRow>
-            <Label htmlFor="howDidYouHear">How did you hear about me?</Label>
-            <Select
-              id="howDidYouHear"
-              name="howDidYouHear"
-              //   value={formData.howDidYouHear}
-              //   onChange={handleInputChange}
-            >
-              <Option value="googleSearch">Google</Option>
-              <Option value="socialMedia">Social Media</Option>
-              <Option value="friend">Friend</Option>
-              <Option value="ad">Ad</Option>
-              <Option value="other">Other</Option>
-            </Select>
-          </FormRow>
-        </FormGroup>
-        <FormButton>
-          <FormRow>
-            <Button type="submit">Submit</Button>
-          </FormRow>
-        </FormButton>
-      </StyledForm>
-      <br />
-      <hr />
+        </>
+      )}
     </>
   );
 }
